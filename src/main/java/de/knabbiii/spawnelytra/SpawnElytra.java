08660e48567cabaffb348c7d634f62e2b1466ae1
SchlangeGoto <kkazimir@protonmail.com>
2026-01-29 11:15:34 +0100

    Add persistent flight and boost state handling through `DataManager`
    - Implement `DataManager` to load and save flying, boosted states, and chestplate data.
    - Integrate data persistence with `SpawnBoostListener` to preserve player states across server restarts.
    - Optimize player tracking by switching from lists to sets and from saving the Player objects to UUIDs.
    
    Signed-off-by: SchlangeGoto <kkazimir@protonmail.com>

--- src/main/java/de/knabbiii/spawnelytra/SpawnElytra.java
+++ src/main/java/de/knabbiii/spawnelytra/SpawnElytra.java
@@ -1,6 +1,7 @@
 package de.knabbiii.spawnelytra;
 
 import de.knabbiii.spawnelytra.commands.SpawnElytraCommand;
+import de.knabbiii.spawnelytra.data.DataManager;
 import de.knabbiii.spawnelytra.listener.SpawnBoostListener;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,12 +21,15 @@ public class SpawnElytra extends JavaPlugin {
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+
+        new DataManager(this);
         
         if (getConfig().getBoolean("enableMetrics", true)) {
             new Metrics(this, 28033);
         }
         
         this.listener = SpawnBoostListener.create(this);
+        listener.loadData();
         getServer().getPluginManager().registerEvents(listener, this);
         
         SpawnElytraCommand commandHandler = new SpawnElytraCommand();
@@ -39,22 +43,25 @@ public class SpawnElytra extends JavaPlugin {
     public void onDisable() {
         if (listener != null) {
             listener.cancel();
+            listener.saveDataSync(); // Use sync version on shutdown
         }
         instance = null;
         getLogger().info("SpawnElytra v" + getDescription().getVersion() + " disabled!");
     }
-    
+
     @Override
     public void reloadConfig() {
         super.reloadConfig();
-        
+
         if (listener != null) {
+            listener.saveData(); // Save before canceling
             listener.cancel();
         }
-        
+
         this.listener = SpawnBoostListener.create(this);
+        listener.loadData(); // Load data into new listener
         getServer().getPluginManager().registerEvents(listener, this);
-        
+
         getLogger().info("SpawnElytra configuration reloaded!");
     }
 }
